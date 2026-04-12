3d007aae593585bec26e84e7251f54ee4621208f
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-12 09:43:25 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-                        cd /home/pi/IMAGEN_PHP                                             
+                        cd /home/pi/IMAGEN_PHP2                                             
                         git pull --force                      
                         sudo rm -R /home/pi/A108
                         mkdir /home/pi/A108                                                
-                        cp -R /home/pi/IMAGEN_PHP/* /home/pi/A108
-                        cp -R /home/pi/IMAGEN_PHP/html/ /var/www/
+                        cp -R /home/pi/IMAGEN_PHP2/* /home/pi/A108
+                        cp -R /home/pi/IMAGEN_PHP2/html/ /var/www/
                         sleep 6                                             
                         sudo chmod 777 -R /home/pi/A108   
                         sudo chmod 777 -R /var/www/html               
