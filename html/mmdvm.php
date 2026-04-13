57056e4af802bd2011b39968047235da7bede969
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-13 23:30:11 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -700,10 +700,10 @@ button.btn-header { font-family: var(--font-mono); }
 <div id="xtModal" class="xterm-modal" onclick="if(event.target===this)xtClose()">
 <div class="xterm-box">
   <div class="xterm-title">⌨ Terminal · EA3EIZ</div>
-  <div class="xterm-out" id="xtOut">pi@raspberry:~$ Terminal lista
+  <div class="xterm-out" id="xtOut">pi@raspberry:~ $ Terminal lista
 </div>
   <div class="xterm-row">
-    <span class="xterm-pr" id="xtPr">pi@raspberry:~$</span>
+    <span class="xterm-pr" id="xtPr">pi@raspberry:~ $</span>
     <input id="xtInp" class="xterm-inp" autocomplete="off" spellcheck="false" placeholder="escribe un comando…">
   </div>
   <div class="restore-btns">
